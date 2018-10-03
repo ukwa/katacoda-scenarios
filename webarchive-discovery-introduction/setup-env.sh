95f9dfdae113ff4520c163d4dd93277ef9681be0
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-10-03 18:37:12 +0100

    Install automatically.

--- webarchive-discovery-introduction/setup-env.sh
+++ webarchive-discovery-introduction/setup-env.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-apt-get install openjdk-8-jre
+apt-get install -y openjdk-8-jre
 wget https://repo1.maven.org/maven2/uk/bl/wa/discovery/warc-indexer/3.0.0/warc-indexer-3.0.0-jar-with-dependencies.jar
 
