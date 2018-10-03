a73393dd22b7c84451f39ae864169abad2990d74
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-10-03 19:49:38 +0100

    And download the JAR.

--- webarchive-discovery-introduction/setup-env.sh
+++ webarchive-discovery-introduction/setup-env.sh
@@ -1,4 +1,5 @@
 apt-get update
 apt-get install -y openjdk-8-jre-headless
-echo curl -O https://repo1.maven.org/maven2/uk/bl/wa/discovery/warc-indexer/3.0.0/warc-indexer-3.0.0-jar-with-dependencies.jar
+cd ~/
+curl -O https://repo1.maven.org/maven2/uk/bl/wa/discovery/warc-indexer/3.0.0/warc-indexer-3.0.0-jar-with-dependencies.jar
 
