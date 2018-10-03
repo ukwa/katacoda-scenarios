c419fa795788abb66c6b35c23de3efb46866621b
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-10-03 20:13:15 +0100

    update warc location.

--- webarchive-discovery-introduction/setup-env.sh
+++ webarchive-discovery-introduction/setup-env.sh
@@ -1,5 +1,5 @@
 apt-get update
 apt-get install -y openjdk-8-jre-headless
-cd ~/
+scd /home/scrapbook/tutorial
 curl -O https://repo1.maven.org/maven2/uk/bl/wa/discovery/warc-indexer/3.0.0/warc-indexer-3.0.0-jar-with-dependencies.jar
 
